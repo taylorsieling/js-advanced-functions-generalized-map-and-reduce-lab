e7bede6f13238ab48da38a0ce4d93f0e9d46de4a
taylorsieling <taylorasieling@gmail.com>
2021-03-18 15:26:03 -0500

    commit tree 61ad0e27153586427b1c241067c571ad6b539375

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Add your functions here
+function map(array, callback) {
+    let newArray = [];
+    array.forEach(element => {
+        newArray.push(callback(element));
+    });
+    return newArray
+}
+
+function reduce(array, callback, startingPoint) {
+    let total = startingPoint != undefined ? startingPoint : 0;
+    let value = array.every(Boolean)
+    array.forEach(element => {
+        total = callback(total, element)
+    })
+    if (total) {return total} else {return value}
+}
